bba299ca550a514f628869c3a8bcf05af6ef3f20
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-27 15:44:29 -0300

    Update server.ts

--- api/server.ts
+++ api/server.ts
@@ -1,7 +1,16 @@
-import app from '.app'
 import dotenv from 'dotenv'
 
+import app from '.app'
+import connect from './src/database'
+
 const PORT: string | number = process.env['PORT'] || 3001;
+
+// connect to mongoDB
+try {
+    connect();
+} catch(err) {
+    console.error(err, 'error mongo connect');
+}
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
