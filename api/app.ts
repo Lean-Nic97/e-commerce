cb7bcafbe33872661eedad1dc6a989b9d577bdc3
_moliNasil <41929823+nahuelmol@users.noreply.github.com>
2021-08-27 15:46:51 -0300

    Update app.ts

--- api/app.ts
+++ api/app.ts
@@ -1,27 +1,16 @@
-import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 
 import routes from './src/routes/index';
-import connect from './src/database';
 
 dotenv.config();
 
 const app = express();
 
-const PORT: string | number = process.env['PORT'] || 3001;
-
 // middleware
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json({ limit: '50mb' }));
 app.use(morgan('dev'));
-
-// connect to mongoDB
-try {
-    connect();
-} catch(err) {
-    console.error(err, 'error mongo connect');
-}
 
 // routes
 app.use('/api', routes);
